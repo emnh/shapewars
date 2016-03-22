ea175e9e2652441db5246a69316ce845608983ca
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2016-03-22 22:41:49 +0100

    fix attack logic. fix health bars.

--- main.js
+++ main.js
@@ -6,6 +6,15 @@ function formatFloat(x, decimals) {
   return x.toFixed(decimals);
 }
 
+function worldToScreen(pos) {
+  var vector = pos.clone().project(data.camera);
+
+  vector.x = (vector.x + 1) / 2 * data.width;
+  vector.y = -(vector.y - 1) / 2 * data.height;
+
+  return new THREE.Vector2(vector.x, vector.y);
+}
+
 function main() {
   data.BaseSize = 50;
   data.BaseOffset = 300;
@@ -20,6 +29,8 @@ function main() {
   // set the scene size
   var WIDTH = window.innerWidth,
     HEIGHT = 600;
+  data.width = WIDTH;
+  data.height = HEIGHT;
 
   // set some camera attributes
   var VIEW_ANGLE = 45,
@@ -61,6 +72,8 @@ function main() {
 
   addLights();
 
+  // necessary for worldToScreen
+  data.renderer.render(data.scene, data.camera);
   addBases();
 
   setupHandlers();
@@ -71,17 +84,26 @@ function main() {
   tick();
 }
 
-function addUnit(spec) {
-  var unit = {};
-  unit.color = spec.team == 0 ? 0xFF0000 : 0x0000FF;
+function addUnit(spec, initial) {
+  var unit = $.extend({}, initial);
+  unit.health = spec.maxHealth;
+  unit.color = unit.team == 0 ? 0xFF0000 : 0x0000FF;
   var sphere = getSphere(spec.size, unit.color);
   data.scene.add(sphere);
   unit.three = sphere;
   unit.spec = spec;
+  unit.healthBar = addProgressBar(unit);
+  unit.setHealth = function(health) {
+    unit.health = health;
+    unit.healthBar.setProgress(unit.health / unit.spec.maxHealth);
+  };
+  unit.getHealth = function() {
+    return unit.health;
+  };
   unit.tick = function() {
     var move = function() {
-      var speed = spec.team == 0 ? spec.speed : -spec.speed;
-      if (spec.team == 0) {
+      var speed = unit.team == 0 ? spec.speed : -spec.speed;
+      if (unit.team == 0) {
         if (unit.three.position.x + speed <= data.xBound) {
           unit.three.position.x += speed;
         }
@@ -97,9 +119,9 @@ function addUnit(spec) {
       for (var i = 0; i < data.units.length; i++) {
         var unit2 = data.units[i];
         if (unit === unit2) continue;
-        if (unit.team === unit2.team) continue;
+        if (unit.team == unit2.team) continue;
         var dist = unit.three.position.distanceTo(unit2.three.position);
-        if (dist < unit2.size + spec.range) {
+        if (dist < unit2.spec.size + spec.range) {
           targets.push(unit2);
         }
       }
@@ -108,7 +130,7 @@ function addUnit(spec) {
     var attack = function(targets) {
       for (var i = 0; i < targets.length; i++) {
         var target = targets[i];
-        target.setHealth(target.getHealth() - spec.damage);
+        target.setHealth(Math.max(target.getHealth() - spec.damage, 0));
         break;
       }
     };
@@ -118,13 +140,13 @@ function addUnit(spec) {
     } else {
       move();
     }
+    unit.healthBar.tick();
   };
   return unit;
 }
 
 function send() {
-  spec = {
-    team: 0,
+  var spec = {
     size: 5,
     speed: 1.0,
     sight: 10,
@@ -133,10 +155,13 @@ function send() {
     health: 100,
     maxHealth: 100,
   };
-  var unit = addUnit(spec);
+  var initial = {
+    team: 0,
+  };
+  var unit = addUnit(spec, initial);
   var offset = 10;
   unit.three.position.x = 
-    spec.team == 0 ?
+    unit.team == 0 ?
     data.base1.three.position.x + data.base1.spec.size + offset : 
     data.base2.three.position.x - data.base2.spec.size - offset;
   unit.three.position.y = (Math.random() * 2.0 - 1.0) * data.yBound;
@@ -193,69 +218,75 @@ function getSphere(radius, color) {
 function addBases() {
   var baseOffset = data.BaseOffset;
   var baseSpec = {
-    team: 0,
     size: data.BaseSize,
     speed: 0,
     sight: 10,
     range: 5,
     damage: 1.0,
-    health: 100,
     maxHealth: 100,
     type: data.UnitType.BASE,
   };
-  var baseSpec1 = $.extend({}, baseSpec);
-  var baseSpec2 = $.extend({}, baseSpec, {
-    team: 1
-  });
-
-  var unit = addUnit(baseSpec1);
+  var initial1 = {
+    team: 0,
+  };
+  var initial2 = {
+    team: 1,
+  };
+  var unit = addUnit(baseSpec, initial1);
   unit.three.position.x = -baseOffset;
   data.units.push(unit);
   data.base1 = unit;
  
-  var health = addBaseHealth();
-  var healthWidth = 300;
-  health.$progress.offset({
-    top: 100,
-    left: 80
-  });
-  health.$progress.width(healthWidth);
-  data.base1.health = health;
-
-  var unit = addUnit(baseSpec2);
+  var unit = addUnit(baseSpec, initial2);
   unit.three.position.x = baseOffset;
   data.units.push(unit);
   data.base2 = unit;
-
-  var health = addBaseHealth();
-  health.$progress.offset({
-    top: 100,
-    left: window.innerWidth - 80 - healthWidth
-  });
-  health.$progress.width(healthWidth);
-  data.base2.health = health;
 }
 
-function addBaseHealth() {
-  progress = {};
+function addProgressBar(unit) {
+  var progress = {};
 
-  $progress = $("<div class='myProgress'/>");
-  $bar = $("<div class='myBar'/>");
-  $text = $("<span/>");
+  var $progress = $("<div class='myProgress'/>");
+  var $bar = $("<div class='myBar'/>");
+  var $text = $("<span/>");
   $progress.append($bar);
   $bar.append($text);
   $("body").append($progress);
 
-  progress.setHealth = function(health) {
-    $bar.width(health * 100 + '%');
-    $text.html(formatFloat(health * 100, 0) + '%');
-    progress.health = health;
+  progress.setProgress = function(completed) {
+    $bar.width(completed * 100 + '%');
+    $text.html(formatFloat(completed * 100, 0) + '%');
+    progress.completed = completed;
   }
-  progress.getHealth = function() {
-    return progress.health;
+  progress.getProgress = function() {
+    return progress.completed;
   }
   progress.$progress = $progress;
-  progress.setHealth(1.0);
+  progress.setProgress(1.0);
+
+  progress.tick = function() {
+    var pos1 = unit.three.position.clone();
+    pos1.x -= unit.spec.size;
+    pos1.y += unit.spec.size;
+    pos1.z += unit.spec.size;
+    var pos2 = unit.three.position.clone();
+    pos2.x += unit.spec.size;
+    pos2.y += unit.spec.size;
+    pos2.z += unit.spec.size;
+
+    var healthWidth = worldToScreen(pos2).x - worldToScreen(pos1).x;
+    var healthHeight = 20;
+    var center = worldToScreen(unit.three.position);
+    var htop = center.y - healthWidth / 2 - healthHeight;
+    var hleft = center.x - healthWidth / 2;
+
+    $progress.offset({
+      top: htop,
+      left: hleft
+    });
+    $progress.width(healthWidth);
+    $progress.height(healthHeight);
+  }
 
   return progress;
 }
